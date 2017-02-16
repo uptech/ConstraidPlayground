aeca37a8018679b776ea2039a6635f65510bfd41
Andrew De Ponte <cyphactor@gmail.com>
2017-02-16 00:10:25 -0800

    Initial XCode iOS Single View skeleton
    Why you made the change:
    
    I did this so that I would have something to start from in creating this playground
    appropriately.

---
+++ ConstraidPlayground/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ConstraidPlayground
+//
+//  Created by Andrew De Ponte on 2/16/17.
+//  Copyright © 2017 UpTech Works, LLC. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
